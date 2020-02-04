cf30699f0ee477e2ece821efa69d85d0a2be7083
geraldgsh <geraldgsh@hotmail.com>
2020-02-05 06:11:51 +0800

    added modular js files and folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello, world")
+import navbar from "./modules/navbar";
+import jumbotron from "./modules/jumbotron";
+import menu from "./modules/menu";
+import gallery from "./modules/gallery";
+import contact from "./modules/contact";
+import socialMedia from "./modules/socialMedia";
+import footer from "./modules/footer";
+
+console.log("Running Webpack");
