27348e3dfca5e2fc5ea8c10d6ca80a043496535b
geraldgsh <geraldgsh@hotmail.com>
2020-02-07 03:20:38 +0800

    completed menu.js

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -42,7 +42,7 @@ const menu = () => {
       title: 'Winter Melon Soup',
       meta: 'Winter Melon / Meatball',
       price: '$9'
-    },
+    }
   ];
 
   const mainItem = [
@@ -68,26 +68,50 @@ const menu = () => {
       price: '$9'
     },
     {
-      link: 'https://thewoksoflife.com/oxtail-soup/',
-      img: 'media/oxtail.png',
-      title: 'Oxtail Soup',
-      meta: 'oxtail / radish',
-      price: '$12'
+      link: 'https://thewoksoflife.com/beef-tofu-stir-fry/',
+      img: 'media/beef-tofu.png',
+      title: 'Stir Fry Beef Tofu',
+      meta: 'Beef / Tofu',
+      price: '$11'
     },
     {
-      link: 'https://thewoksoflife.com/red-vegetable-soup-shanghai-luo-song-tang/',
-      img: 'media/red-soup.png',
-      title: 'Red Vegetable Soup',
-      meta: 'Oxtail / Tomato',
+      link: 'https://thewoksoflife.com/cashew-shrimp-stir-fry/',
+      img: 'media/cashew-shrimp.png',
+      title: 'Cashew Shrimp Stir Fry',
+      meta: 'Cashew Nuts / Shrimp / Celery',
       price: '$13'
     },
     {
-      link: 'https://thewoksoflife.com/chinese-winter-melon-soup-meatballs/',
-      img: 'media/winter-melon.png',
-      title: 'Winter Melon Soup',
-      meta: 'Winter Melon / Meatball',
-      price: '$9'
+      link: 'https://thewoksoflife.com/sweet-sour-fish-fillet/',
+      img: 'media/sweet-sour-fish.png',
+      title: 'Sweet Sour Fish Fillet',
+      meta: 'Fish Fillet / Pineapple / vinegar',
+      price: '$12'
+    }
+  ];
+
+  const dessertItem = [
+    {
+      link: 'https://thewoksoflife.com/red-bean-soup/',
+      img: 'media/red-bean.png',
+      title: 'Red Bean Soup',
+      meta: 'Red Bean',
+      price: '$5'
     },
+    {
+      link: 'https://thewoksoflife.com/snow-fungus-soup-pears/',
+      img: 'media/fungus.png',
+      title: 'Snow Fungus Soup',
+      meta: 'Fungus / Asian Pear',
+      price: '$11'
+    },
+    {
+      link: 'https://thewoksoflife.com/baobing-chinese-shaved-ice/',
+      img: 'media/shaved-ice.png',
+      title: 'Baobing Shaved Ice',
+      meta: 'Mango / Taro / Red Bean',
+      price: '$7'
+    }
   ];
 
   const amenu = document.createElement('a');
@@ -125,19 +149,19 @@ const menu = () => {
   col2.classList.add('col-lg-4', 'col-md-4', 'col-sm-6', 'col-xs-12', 'mb40');
   row2.appendChild(col2);
 
-  const menuBlock =  document.createElement('div');
-  menuBlock.classList.add('menu-block');
-  col2.appendChild(menuBlock);
+  const menuBlock1 =  document.createElement('div');
+  menuBlock1.classList.add('menu-block');
+  col2.appendChild(menuBlock1);
 
   const menuTitle1 = document.createElement('h3');
   menuTitle1.classList.add('menu-title');
   menuTitle1.innerHTML = 'Starter';
-  menuBlock.appendChild(menuTitle1);
+  menuBlock1.appendChild(menuTitle1);
 
   starterItem.forEach(soup=> {
     const menuContent = document.createElement('div');
     menuContent.classList.add('menu-content');
-    menuBlock.appendChild(menuContent);
+    menuBlock1.appendChild(menuContent);
 
     const row = document.createElement('div');
     row.classList.add('row');
@@ -191,6 +215,150 @@ const menu = () => {
     dishPrice.appendChild(priceTag);    
 
   });
+
+  
+  const col5 = document.createElement('div');
+  col5.classList.add('col-lg-4', 'col-md-4', 'col-sm-6', 'col-xs-12', 'mb40');
+  row2.appendChild(col5);
+
+  const menuBlock2 =  document.createElement('div');
+  menuBlock2.classList.add('menu-block');
+  col5.appendChild(menuBlock2);
+
+  const menuTitle2 = document.createElement('h3');
+  menuTitle2.classList.add('menu-title');
+  menuTitle2.innerHTML = 'Main Course';
+  menuBlock2.appendChild(menuTitle2);
+
+  mainItem.forEach(main=> {
+    const menuContent = document.createElement('div');
+    menuContent.classList.add('menu-content');
+    menuBlock2.appendChild(menuContent);
+
+    const row = document.createElement('div');
+    row.classList.add('row');
+    menuContent.appendChild(row);
+
+    const col6 = document.createElement('div');
+    col6.classList.add('col-lg-3', 'col-md-3', 'col-sm-3', 'col-xs-12');
+    row.appendChild(col6);
+
+    const dishImg = document.createElement('div');
+    dishImg.classList.add('dish-img');
+    col6.appendChild(dishImg);
+
+    const aTag = document.createElement('a');
+    aTag.setAttribute('href', main.link);
+    dishImg.appendChild(aTag);
+
+    const img = document.createElement('img');
+    img.setAttribute('src', main.img)
+    img.setAttribute('alt', main.title);
+    img.classList.add('img-circle')
+    aTag.appendChild(img);
+
+    const col7 = document.createElement('div');
+    col7.classList.add('col-lg-9', 'col-md-9', 'col-sm-9', 'col-xs-12');
+    row.appendChild(col7);
+    const dishContent = document.createElement('div');
+    dishContent.classList.add('dish-content');
+    col7.appendChild(dishContent);
+
+    const dishTitle = document.createElement('h5');
+    dishTitle.classList.add('dish-title');
+    dishTitle.innerHTML = `<a href='#'>${main.title}</a>`
+    dishContent.appendChild(dishTitle);
+    const dishLink = document.createElement('a');
+    dishLink.setAttribute('href', '#')
+    dishLink.inner = main.title;
+    dishTitle.appendChild(dishLink);
+
+    const dishMeta = document.createElement('span');
+    dishMeta.classList.add('dish-meta');
+    dishMeta.innerHTML = main.meta
+    dishContent.appendChild(dishMeta);
+
+    const dishPrice = document.createElement('div');
+    dishPrice.classList.add('dish-price');
+    dishContent.appendChild(dishPrice);
+
+    const priceTag =  document.createElement('p');
+    priceTag.innerHTML = main.price;
+    dishPrice.appendChild(priceTag);    
+
+  });
+
+  const col8 = document.createElement('div');
+  col8.classList.add('col-lg-4', 'col-md-4', 'col-sm-6', 'col-xs-12', 'mb40');
+  row2.appendChild(col8);
+
+  const menuBlock3 =  document.createElement('div');
+  menuBlock3.classList.add('menu-block');
+  col8.appendChild(menuBlock3);
+
+  const menuTitle3 = document.createElement('h3');
+  menuTitle3.classList.add('menu-title');
+  menuTitle3.innerHTML = 'Dessert';
+  menuBlock3.appendChild(menuTitle3);
+
+  dessertItem.forEach(dessert => {
+    const menuContent = document.createElement('div');
+    menuContent.classList.add('menu-content');
+    menuBlock3.appendChild(menuContent);
+
+    const row = document.createElement('div');
+    row.classList.add('row');
+    menuContent.appendChild(row);
+
+    const col9 = document.createElement('div');
+    col9.classList.add('col-lg-3', 'col-md-3', 'col-sm-3', 'col-xs-12');
+    row.appendChild(col9);
+
+    const dishImg = document.createElement('div');
+    dishImg.classList.add('dish-img');
+    col9.appendChild(dishImg);
+
+    const aTag = document.createElement('a');
+    aTag.setAttribute('href', dessert.link);
+    dishImg.appendChild(aTag);
+
+    const img = document.createElement('img');
+    img.setAttribute('src', dessert.img)
+    img.setAttribute('alt', dessert.title);
+    img.classList.add('img-circle')
+    aTag.appendChild(img);
+
+    const col10 = document.createElement('div');
+    col10.classList.add('col-lg-9', 'col-md-9', 'col-sm-9', 'col-xs-12');
+    row.appendChild(col10);
+    const dishContent = document.createElement('div');
+    dishContent.classList.add('dish-content');
+    col10.appendChild(dishContent);
+
+    const dishTitle = document.createElement('h5');
+    dishTitle.classList.add('dish-title');
+    dishTitle.innerHTML = `<a href='#'>${dessert.title}</a>`
+    dishContent.appendChild(dishTitle);
+    const dishLink = document.createElement('a');
+    dishLink.setAttribute('href', '#')
+    dishLink.inner = dessert.title;
+    dishTitle.appendChild(dishLink);
+
+    const dishMeta = document.createElement('span');
+    dishMeta.classList.add('dish-meta');
+    dishMeta.innerHTML = dessert.meta
+    dishContent.appendChild(dishMeta);
+
+    const dishPrice = document.createElement('div');
+    dishPrice.classList.add('dish-price');
+    dishContent.appendChild(dishPrice);
+
+    const priceTag =  document.createElement('p');
+    priceTag.innerHTML = dessert.price;
+    dishPrice.appendChild(priceTag);    
+
+  });
+
 
   return foodlist;
 }
